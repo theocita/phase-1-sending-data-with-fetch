82d6ce1e9e41129615d36c0b0282d275ac0be675
theocita <theocitaallen@gmail.com>
2022-10-22 19:05:26 -0400

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+
+function submitData(name, email){
+    fetch('http://localhost:3000/users',{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept" : "application/json"
+    },
+    body: JSON.stringify({name, email})
+})
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(function (error){
+    alert('Unauthorized Access');
+    console.log(error.message)
+})
+}
